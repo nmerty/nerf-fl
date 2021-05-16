40cb17222e128257c02c736d65e1f373842820eb
Dávid Komorowicz <dawars00@gmail.com>
2021-05-16 18:01:51 +0200

    Change perturbation

--- models/poses.py
+++ models/poses.py
@@ -20,7 +20,7 @@ class LearnPose(nn.Module):
         if init_c2w is not None:
             self.init_c2w = nn.Parameter(init_c2w, requires_grad=False)
 
-        self.perturbation = nn.Parameter(convert3x4_4x4(torch.normal(0, perturb_sigma, [num_cams, 3, 4])), requires_grad=False) if perturb_sigma > 0 else None
+        self.perturbation = nn.Parameter(torch.normal(0, perturb_sigma, [num_cams, 6]), requires_grad=False) if perturb_sigma > 0 else None
 
         self.r = nn.Parameter(torch.zeros(size=(num_cams, 3), dtype=torch.float32), requires_grad=learn_R)  # (N, 3)
         self.t = nn.Parameter(torch.zeros(size=(num_cams, 3), dtype=torch.float32), requires_grad=learn_t)  # (N, 3)
@@ -28,13 +28,15 @@ class LearnPose(nn.Module):
     def forward(self, cam_id):
         r = self.r[cam_id]  # (3, ) axis-angle
         t = self.t[cam_id]  # (3, )
+        if self.perturbation is not None:
+            r = r + self.perturbation[cam_id, :3]
+            t = t + self.perturbation[cam_id, 3:]
+
         c2w = make_c2w(r, t)  # (4, 4)
 
         # learn a delta pose between init pose and target pose, if a init pose is provided
         if self.init_c2w is not None:
             c2w = c2w @ self.init_c2w[cam_id]
 
-        if self.perturbation is not None:
-            return c2w + self.perturbation[cam_id]
 
         return c2w
